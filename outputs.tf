9a5b57418c157162508c76ff0635ee0be2050393
Devin Slate <sl8t3@hotmail.com>
2022-11-11 12:46:33 -0700

    outputs for priv and pub subnets

--- outputs.tf
+++ outputs.tf
@@ -1,3 +1,14 @@
 output "vpc_id" {
+  type  = string
   value = aws_vpc.main.id
+}
+
+output "pub_subnets" {
+  type  = list(string)
+  value = aws_subnet.pub.*.id
+}
+
+output "priv_subnets" {
+  type  = list(string)
+  value = aws_subnet.priv.*.id
 }
