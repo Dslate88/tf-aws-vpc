dce2697d6626882bc5279fd54ae63e9ea7f42a6e
Devin Slate <sl8t3@hotmail.com>
2022-11-11 13:01:00 -0700

    added more outputs

--- outputs.tf
+++ outputs.tf
@@ -1,14 +1,39 @@
 output "vpc_id" {
-  type  = string
   value = aws_vpc.main.id
 }
 
 output "pub_subnets" {
-  type  = list(string)
-  value = aws_subnet.pub.*.id
+  value = aws_subnet.public.*.id
 }
 
 output "priv_subnets" {
-  type  = list(string)
-  value = aws_subnet.priv.*.id
+  value = aws_subnet.private.*.id
+}
+
+output "pub_route_table" {
+  value = aws_route_table.public.id
+}
+
+output "priv_route_table" {
+  value = aws_route_table.private.id
+}
+
+output "vpc_cidr" {
+  value = aws_vpc.main.cidr_block
+}
+
+output "pub_subnet_cidrs" {
+  value = aws_subnet.public.*.cidr_block
+}
+
+output "priv_subnet_cidrs" {
+  value = aws_subnet.private.*.cidr_block
+}
+
+output "pub_route_table_id" {
+  value = aws_route_table.public.id
+}
+
+output "priv_route_table_id" {
+  value = aws_route_table.private.id
 }
